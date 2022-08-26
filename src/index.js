2a9c226d1ba2b904e92ef1f0c819b60db69e8565
Gatteber <Garrett.Atteberry@gmail.com>
2022-08-26 10:28:38 +0900

    added basic fetch function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './style.scss';
 
-console.log('Hello, World!');
+const mathConversions = (() => {
+  const toCelsius = (kTemp) => kTemp - 273.15;
+
+  return { toCelsius };
+})();
+
+const dataModule = (() => {
+  const baseLoc = async () => {
+    const getBaseLoc = await fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=Osaka&APPID=69f5f1430b2393d54d2d24a880e45502',
+      { mode: 'cors' }
+    );
+    getBaseLoc.json().then((response) => {
+      console.log(
+        mathConversions.toCelsius(response.main.temp) + ` Degrees Celsius`
+      );
+    });
+  };
+  baseLoc();
+})();
+
+export default { dataModule };
