ef73df3aabbffde90ef55a30fa3a0c606c7ea912
Gatteber <Garrett.Atteberry@gmail.com>
2022-08-29 18:59:12 +0900

    added basic search function and retrieval from API

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ const mathConversions = (() => {
   return { toCelsius };
 })();
 
+const displayController = (() => {
+  const renderTemp = (temp) => {
+    const getTemp = document.querySelector('.current-temp');
+    getTemp.innerHTML = `Current Temperature: ${temp} °C`;
+  };
+
+  const renderLoc = (loc) => {
+    const getLoc = document.querySelector('.current-loc');
+    getLoc.innerHTML = `Current Location: ${loc}`;
+  };
+  return { renderTemp, renderLoc };
+})();
+
 const dataModule = (() => {
   const baseLoc = async () => {
     const getBaseLoc = await fetch(
@@ -13,12 +26,36 @@ const dataModule = (() => {
       { mode: 'cors' }
     );
     getBaseLoc.json().then((response) => {
-      console.log(
-        mathConversions.toCelsius(response.main.temp) + ` Degrees Celsius`
+      //   console.log(response);
+      displayController.renderTemp(
+        mathConversions.toCelsius(response.main.temp).toFixed(1)
       );
+      displayController.renderLoc(response.name);
     });
   };
   baseLoc();
+
+  const newLoc = async (val) => {
+    const getNewLoc = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${val}&APPID=69f5f1430b2393d54d2d24a880e45502`,
+      { mode: 'cors' }
+    );
+    getNewLoc.json().then((response) => {
+      displayController.renderTemp(response.main.temp);
+      displayController.renderLoc(response.name);
+    });
+  };
+  return { baseLoc, newLoc };
+})();
+
+const newItem = (() => {
+  const getForm = document.getElementById('submitButton');
+  getForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    const getData = document.getElementById('newSearch');
+    dataModule.newLoc(getData.value);
+    getData.value = null;
+  });
 })();
 
 export default { dataModule };
